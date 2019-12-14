3e92c6c02c94d6fc0c1757925bd3211670d936a7
fadeomar <fadeomar2015@gmail.com>
2019-12-14 20:35:43 +0200

    Updates

---
+++ precache-manifest.a4c2a2d3b2034e718fce1c2ce1c9ef85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2cb387a4fc8aed6f570b634875f64e7",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "2e14480b1ee7653d4096",
+    "url": "/todo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a4882f37ecd05e406240",
+    "url": "/todo/static/js/2.8e7d6ab2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo/static/js/2.8e7d6ab2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2e14480b1ee7653d4096",
+    "url": "/todo/static/js/main.7b2db1c9.chunk.js"
+  },
+  {
+    "revision": "ed044effc7b6bca18d34",
+    "url": "/todo/static/js/runtime-main.334cb69f.js"
+  }
+]);
